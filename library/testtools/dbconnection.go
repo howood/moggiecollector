1197b5a8ff10c385212e57911f7536ed552b2acb
howood <>
2025-01-06 21:18:11 +0900

    refactoring

---
+++ library/testtools/dbconnection.go
@@ -0,0 +1,35 @@
+package testtools
+
+import (
+	"context"
+	"testing"
+
+	"github.com/howood/moggiecollector/infrastructure/client"
+	"gorm.io/gorm"
+)
+
+func DBTx(t *testing.T) *gorm.DB {
+	t.Helper()
+
+	ctx := context.Background()
+	dataaccessor := client.NewDatastorAssessor()
+
+	tx := dataaccessor.Instance.GetClient().WithContext(ctx).Begin()
+	if tx.Error != nil {
+		t.Fatalf("Failed to start a transaction: %v", tx.Error)
+	}
+
+	t.Cleanup(func() {
+		if r := tx.Rollback(); r.Error != nil {
+			t.Fatalf("Failed to rollback the transaction: %v", r.Error)
+		}
+
+		sqlDB, _ := dataaccessor.Instance.GetClient().DB()
+
+		if err := sqlDB.Close(); err != nil {
+			t.Fatalf("Failed to close the connection %v", err)
+		}
+	})
+
+	return tx
+}
