cb987a6f8f68b3a3bca56f1c50ece404d7adf35d
howood <>
2025-01-07 20:51:45 +0900

    refactoring

--- domain/model/user.go
+++ domain/model/user.go
@@ -11,12 +11,12 @@ const (
 
 // User entity
 type User struct {
-	UserID    uint64 `gorm:"primary_key"  json:"user_id"     v-post:"omitempty" v-put:"omitempty"`
-	Name      string `json:"name"         v-post:"required"  v-put:"required"`
-	Email     string `gorm:"index:email"  json:"email"       v-post:"required"  v-put:"required"`
-	Password  string `json:"-"            v-post:"required"  v-put:"required"`
-	Salt      string `json:"-"            v-post:"omitempty" v-put:"omitempty"`
-	Status    int64  `gorm:"index:status" json:"status"      v-post:"required"  v-put:"required"`
+	UserID    uint64 `gorm:"primary_key"`
+	Name      string
+	Email     string `gorm:"index:email"`
+	Password  string
+	Salt      string
+	Status    int64 `gorm:"index:status"`
 	CreatedAt time.Time
 	UpdatedAt time.Time
 }
